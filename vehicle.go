41168c44cf91fc426ee9a744fa6b4845a0b42d08
Mihai <mihaiiftode@users.noreply.github.com>
2021-07-06 20:38:45 +0000

    Add module3

--- vehicle.go
+++ vehicle.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -73,6 +74,19 @@ func main() {
 	// Generate ratings for the different vehicles
 	generateRating()
 	// Print ratings for the different vehicles
+
+	for _, veh := range inventory {
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+	}
 }
 
 func readJSONFile() Values {
@@ -130,4 +144,36 @@ func generateRating() {
 
 		vehicleResult[v.Name] = vehResult
 	}
+}
+
+func showrating(model string) {
+	ratingFound := false
+
+	for m, r := range vehicleResult {
+		if m == model {
+			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
+		}
+	}
+
+	if !ratingFound {
+		fmt.Printf("No rating for this vehicle")
+	}
+}
+
+func (c *car) carDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Car", c.make, c.model)
+
+	showrating(c.model)
+}
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Bike", b.make, b.model)
+
+	showrating(b.model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v ", "Truck", t.make, t.model)
+
+	showrating(t.model)
 }
